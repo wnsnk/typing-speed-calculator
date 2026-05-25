e94900c1f38764a368bedff0ae7409d301b2cb86
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-05-25 18:17:49 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from words import RandomWordsGenerator
+
+score = 0
+
+random_words_generator = RandomWordsGenerator()
+words = random_words_generator.random_words
+
+print(words)
+root = Tk()
+root.config(background='white')
+
+score_label = Label(root, text=f'Score: {score}', background='white',)
+score_label.pack()
+
+label = Label(root, background='white',
+              text='Typing Speed Calculator!', font=('Arial', 30, 'bold'))
+label.pack()
+
+explanation_label = Label(root, background='white',
+                          text='Type the word on the screen as fast and accurate as you can!')
+explanation_label.pack()
+
+focus_word = Label(root, background='light blue',
+                   text=words[0], font=('Arial', 20, 'bold'))
+focus_word.pack()
+words_to_type = Label(root, background='light blue',
+                      text=words[1:10], wraplength=1000)
+words_to_type.pack()
+
+entry = Entry()
+entry.pack()
+entry.focus()
+
+
+def next_word(event):
+    global score, words
+    typed_word = entry.get().strip()
+    current_word = words[0]
+    if typed_word == current_word:
+        score += 1
+    else:
+        print('no')
+    words.remove(current_word)
+    score_label.config(text=f'Score: {score}')
+    focus_word.config(text=words[0])
+    words_to_type.config(text=words[1:10])
+    root.update()
+    root.update_idletasks()
+    entry.delete(0, END)
+
+
+root.bind('<space>', next_word)
+# root.bind('<return>', next_word)
+
+root.mainloop()
---
+++ words.py
@@ -0,0 +1,71 @@
+import time
+import random
+symbols = ["'", '-', '!', '@', "#", '$',
+           '%', '^', '&', '*', '(', ')', '_', '+', '=', ' ', '¿', '¡', '.']
+accent_letters = ['à', 'è', 'ì', 'ò', 'ù', 'À', 'È', 'Ì', 'Ò', 'Ù', 'á', 'é', 'í', 'ó', 'ú', 'ý', 'Á', 'É', 'Í', 'Ó',
+                  'Ú', 'Ý', 'â', 'ê', 'î', 'ô', 'û', 'Â', 'Ê', 'Î', 'Ô', 'Û', 'ä', 'ë', 'ï', 'ö', 'ü', 'ÿ', 'Ä', 'Ë',
+                  'Ï', 'Ö', 'Ü', 'Ÿ', 'ã', 'ñ', 'õ', 'Ã', 'Ñ', 'Õ', 'ß', 'å', 'Å', 'æ', 'Æ', 'œ', 'Œ', 'ç', 'Ç', 'ð',
+                  'Ð',  'ø', 'Ø']
+
+try:
+    with open('cleaned_words.txt', 'r') as cleaned_words:
+        wordlist = cleaned_words.read().splitlines()
+
+except FileNotFoundError:
+    with open('wordlist.txt', 'r') as wordlist_txt:
+        wordlist = wordlist_txt.read().splitlines()
+
+    for num in range(10):
+        num = str(num)
+        symbols. append(num)
+
+    def clean_list(symbols: list, w_list: list):
+        for character in symbols:
+            for word in w_list:
+                if len(word) < 4 or len(word) > 12:
+                    try:
+                        w_list.remove(word)
+                    except ValueError:
+                        print('Already removed')
+                    else:
+                        print(f' Removed: {word}')
+                elif character in word:
+                    try:
+                        w_list.remove(word)
+                    except ValueError:
+                        print(f'already deleted word: {word}')
+                    else:
+                        print(f'Removed: {word}')
+
+        return w_list
+
+    def clean_list_extreme(symbols, w_list):
+        length_wordlist = len(w_list)
+        fully_cleaned = False
+        count = 0
+        while not fully_cleaned:
+            count += 1
+            w_list = clean_list(symbols=symbols, w_list=w_list)
+            print('Total:', len(w_list))
+            if length_wordlist == len(w_list):
+                fully_cleaned = True
+                return w_list
+            else:
+                length_wordlist = len(w_list)
+            print('Round:', count)
+        print('done')
+
+    wordlist = clean_list_extreme(symbols=symbols, w_list=wordlist)
+    wordlist = clean_list_extreme(symbols=accent_letters, w_list=wordlist)
+
+    with open('cleaned_words.txt', 'a') as cleaned_words_txt:
+        for word in wordlist:
+            cleaned_words_txt.write(word)
+            cleaned_words_txt.write('\n')
+
+
+class RandomWordsGenerator():
+    def __init__(self):
+        self.random_words = []
+        for _ in range(120):
+            self.random_words.append(random.choice(wordlist))
